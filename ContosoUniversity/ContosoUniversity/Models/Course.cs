58fb0a7b3b1f00693249da214cb75797c093f5c4
Ingvar Derešivski <inde999@gmail.com>
2022-09-22 15:51:37 +0300

    init

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, E, F
+    }
+}
