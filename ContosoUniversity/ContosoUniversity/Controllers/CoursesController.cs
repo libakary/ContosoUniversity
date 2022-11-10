0c95e5c1dd8d7c64fc7f5c98dccfceb161e415d3
Karol <libakary@users.noreply.github.com>
2022-11-10 10:22:46 +0200

    Courses page functions

--- ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
@@ -1,7 +1,10 @@
 ﻿using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System.Linq;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Controllers
 {
@@ -27,6 +30,144 @@ namespace ContosoUniversity.Controllers
 
             //peaks returnima vaate
             return View(courses);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courses = await _context.Courses
+                .Include(_c => _c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (courses == null)
+            {
+                return NotFound();
+            }
+
+            return View(courses);
+        }
+
+        public IActionResult Create()
+        {
+            PopulateDepartmentsDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseID, Title, Credits, Id")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDepartmentsDropDownList(course.DepartmentId);
+            return View(course);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            PopulateDepartmentsDropDownList(course.DepartmentId);
+            return View(course);
+        }
+
+        [HttpPost, ActionName ("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            
+            }
+            var courseToUpdate = await _context.Courses
+                .FirstOrDefaultAsync(c => c.CourseID == id);
+
+            if (await TryUpdateModelAsync<Course>(courseToUpdate,
+                "",
+                c => c.Credits, c => c.DepartmentId, c => c.Title))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save ćhanges. " +
+                        "Try again, and if the problem persists, " +
+                        "see on your system adminsistraator.");
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDepartmentsDropDownList(courseToUpdate.DepartmentId);
+            return View(courseToUpdate);         
+                
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+            
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var depQuery = from d in _context.Department
+                           orderby d.Name
+                           select d;
+            ViewBag.Id = new SelectList(depQuery.AsNoTracking(), "Id", "Name", selectedDepartment);
         }
     }
 }
